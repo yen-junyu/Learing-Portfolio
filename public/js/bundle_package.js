874abc29644ee3802b764df4cfb39b02e8eb4ca9
yen <yen.cs09g@nctu.edu.tw>
2022-03-23 15:08:09 +0800

    ethers add

--- public/js/bundle_package.js
+++ public/js/bundle_package.js
@@ -3,6 +3,7 @@ var elliptic = require('elliptic');
 var ecies = require("eth-ecies");
 var {encrypt } = require("eth-sig-util")
 var {KEYUTIL} = require('jsrsasign');
+var {ethers} = require("ethers")
 
 const EC = elliptic.ec;
 const ecdsaCurve = elliptic.curves['p256'];
@@ -29,6 +30,7 @@ global.window.Buffer = Buffer
 global.window.ecdsa = ecdsa
 global.window.encrypt_s = encrypt
 global.window.KEYUTIL = KEYUTIL
+global.window.ethers = ethers
 
 
 
